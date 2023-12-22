4036c91c3833712f6db9406be1dbfa6a1e6e6c15
Kirill Sermyagin <sermyagin.abcen7@gmail.com>
2023-12-22 09:03:37 +0300

    Fixed task 4

--- 4/solve.py
+++ 4/solve.py
@@ -2,7 +2,8 @@ def reverse_string(row_to_reverse: str) -> str:
     return row_to_reverse[::-1]
 
 def is_palindrome(row: str) -> bool:
-    return row == row[::-1]
+    return row.lower() == row.lower()[::-1]
 
 print(reverse_string("abcd"), is_palindrome("abcd"))
 print(reverse_string("abba"), is_palindrome("abba"))
+print(reverse_string("aBBa"), is_palindrome("aBBa"))
